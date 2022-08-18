d81529da00eda8a7165da0b508028174bb5b59d0
Kodiak <77172196+ohKodiak@users.noreply.github.com>
2022-08-18 13:09:55 -0500

    complete reupload

--- src/App.js
+++ src/App.js
@@ -1,65 +1,20 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import FileCard from './components/FileCard';
-
-
-
-
-
-
-function App() {
-  const folders = [
-    {name: 'Dad', classStyle: '1', Default: true, id: 123 },
-    {name: 'Kendall', classStyle: '2', Default: false, id: 153 },
-    {name: 'Cat', classStyle: '3', Default: false, id: 133 }
-  ]
-
-
-  // function myTest(folders){
-  //  let newFolders = folders.map((folder) => {
-  //   folder.name = 'test'
-  //    return folder
-  //  })
-  //  console.log(newFolders)
-  // }
-  
-
-  return (
-  
-    <div className="App">
-      
-     <div className='container' >
-         
-      <div className='container-rim'>
-
-    {
-      folders.map((folder)=> {
-        return <FileCard 
-        
-        
-        onFolderClick={(id)=> console.log(id)}
-        name={folder.name} 
-        classStyle={folder.classStyle} 
-        Default={folder.Default} 
-        id={folder.id}/>
-      })
-    }
-
-     {/* <FileCard noteNum='1'/>
-     <FileCard noteNum='2'/>
-     <FileCard noteNum='3'/> */}
-
-
-
-     </div>
-     </div>
-     
-    
-    </div>
-    
-    
-    
-  );
+import React, { Component } from 'react';
+import {  BrowserRouter,  Routes,  Route} from "react-router-dom";
+ 
+import Home from './components/Home';
+import File from './components/File';
+ 
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+            <Routes>
+             <Route path="/" element={<Home />} />
+             <Route path = "/files" element={<File />} />
+            </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-
+ 
 export default App;
