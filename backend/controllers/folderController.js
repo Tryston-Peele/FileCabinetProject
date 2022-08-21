1ebbc1991b040a8bbf07deaf371fa2f93d9acaca
Kodiak <77172196+ohKodiak@users.noreply.github.com>
2022-08-21 12:52:20 -0500

    finished backend skeleton, i havent added anything concerning the users, needs testing with postman

--- backend/controllers/folderController.js
+++ backend/controllers/folderController.js
@@ -20,7 +20,7 @@ const setFolder = asyncHandler(async(req, res) => {
     }
     const folders = await Folder.create({
         text: req.body.text,
-        user: req.user.id,
+       // user: req.user.id,
       })
     
       res.status(200).json(folders)
@@ -30,17 +30,61 @@ const setFolder = asyncHandler(async(req, res) => {
 //@route Get /files
 //@access
 
-const updateFolder = asyncHandler(async(req, res) => {
-    res.status(200).json({message: `update goal ${req.params.id}`})
-})
+const updateFolder = asyncHandler(async (req, res) => {
+    const folders = await Folder.findById(req.params.id)
+  
+    if (!folders) {
+      res.status(400)
+      throw new Error('Folder not found')
+    }
+  
+    // Check for user
+ /*   if (!req.user) {
+      res.status(401)
+      throw new Error('User not found')
+    } */
+  
+    // Make sure the logged in user matches the goal user
+/*    if (goal.user.toString() !== req.user.id) {
+      res.status(401)
+      throw new Error('User not authorized')
+    } */
+  
+    const updatedFolder = await Folder.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+  
+    res.status(200).json(updatedFolder)
+  })
 
 //desc: delete folder
 //@route Get /files
 //@access
 
 const deleteFolder = asyncHandler(async(req,res) => {
-    res.status(200).json({message: `update goal ${req.params.id}`})
-})
+    const folders = await Folder.findById(req.params.id)
+
+    if (!folders) {
+      res.status(400)
+      throw new Error('Goal not found')
+    }
+  
+    // Check for user
+ /*   if (!req.user) {
+      res.status(401)
+      throw new Error('User not found')
+    }
+  
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== req.user.id) {
+      res.status(401)
+      throw new Error('User not authorized')
+    } */
+  
+    await folders.remove()
+  
+    res.status(200).json({ id: req.params.id })
+  })
 
 
 
